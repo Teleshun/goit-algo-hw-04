bc692058f10182ec9fa02de3ddd2ef887e2c5555
Teleshun <108687533+Teleshun@users.noreply.github.com>
2024-03-18 19:16:40 +0200

    Update ex_4.py

--- ex_4.py
+++ ex_4.py
@@ -1,7 +1,7 @@
 def parse_input(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.strip().lower()
-    return cmd, *args
+    return cmd, args 
 
 def add_contact(args, contacts):
     name, phone = args
@@ -15,18 +15,34 @@ def change_contact(args, contacts):
     contacts[name] = new_phone
     return f"Contact '{name}' updated successfully"
 
-
-
 def show_contact(args, contacts):
-    name, _ = args
-    return contacts[name]
+    name = ' '.join(args)
+    return contacts.get(name, "Contact not found")
+
+
+def show_all_contacts(contacts):
+    if not contacts:
+        return "No contacts found."
+    else:
+        contacts_str = ""
+        for name, phone in contacts.items():
+            contacts_str += f"{name}: {phone}\n"
+        return contacts_str.strip()  
+
+def show_phone(args, contacts):
+    name = args[0]
+    phone = contacts.get(name)
+    if phone is not None:
+        return f"{name}'s phone number is: {phone}"
+    else:
+        return f"Contact '{name}' not found."
 
 def main():
     contacts = {}
     print("Welcome to the assistant bot!")
     while True:
         user_input = input("Enter a command: ")
-        command, *args = parse_input(user_input)
+        command, args = parse_input(user_input)
 
         if command in ["close", "exit"]:
             print("Good bye!")
@@ -43,6 +59,12 @@ def main():
 
         elif command == "show":
             print(show_contact(args, contacts))
+
+        elif command == "phone":
+            print(show_phone(args, contacts))
+
+        elif command == "all":
+            print(show_all_contacts(contacts))
 
         else:
             print("Invalid command.")
